2e5dbc6cdac6df18aeb89971cbf200f076c1d068
NetMet1 <36077940+netmet1@users.noreply.github.com>
2023-08-23 18:13:50 -0400

    clean up send_logs old code

--- modules/troubleshoot/send_logs.py
+++ modules/troubleshoot/send_logs.py
@@ -280,12 +280,6 @@ class Send():
                         self.functions.print_paragraphs([
                             [f"{count}     ",0,"yellow"], [self.profile,0,"magenta"], ["=>",0,"white","bold"], [file[file_name_index],1]
                         ])
-                        # print(
-                        #     colored(f"  {count}".ljust(5),"yellow"),
-                        #     colored(self.profile,"magenta"),
-                        #     colored("=>","white",attrs=["bold"]),
-                        #     colored(file[file_name_index],"cyan")
-                        # )
                         count = count + 1
                         dir_list.append(file[file_name_index])
                         
